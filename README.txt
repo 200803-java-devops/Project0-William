4dd38a5c5c9edc0c0bb65dd92787aae1534adda2
William Hibbard <hibbard.wm@gmail.com>
2020-08-08 10:27:43 -0400

    File output added and tested

--- README.txt
+++ README.txt
@@ -6,7 +6,7 @@ FEATURES
 	-using input, returns share price from SQL DB
 	-Can currently take input from a .txt file's contents if the contents are just a single ticker.
 	*-more functionality in this are will be added later.
--Can spit output into console, ticker with share price
+-Can spit output into output.txt, ticker with share price
 	-Pulls this from DB.
 	*-Consider making it so that data is pulled from DB (maybe all entered
 	  manually for now, maybe update upon program launch later) and spit
--- src/Package/App.java
+++ src/Package/App.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 import Package.SQL;
@@ -16,27 +17,41 @@ public class App {
 		String controller = "Yes";
 		Scanner sc = new Scanner(System.in);
 		
+		Scanner scanner = new Scanner( new File("input.txt") );
+		String text = scanner.useDelimiter("\\A").next();
+		FileWriter writer = new FileWriter("output.txt", true);
+
+		
 		try { 
-			Scanner scanner = new Scanner( new File("input.txt") );
-			String text = scanner.useDelimiter("\\A").next();
 //Adding a switch statement in place of the while could add some functionality.
 		while(controller.equals("Yes")) {
 			
 			 
-			 
+		//**Code for console ticker input
 			  //System.out.println("Please type a stock ticker symbol and I will return its share price");
 			  //String ticker = sc.nextLine();
+			
+		//**Process of confirming in console what ticker was found and also returning share price for confirmation
 			  System.out.println(text);
 			  System.out.println(SQL.getStock(text).getPrice());
 			  
-			 			
+		//**Writing to output.txt	 
+			  writer.write("Ticker: ");
+			  writer.write(text);
+			  writer.write("\r\n");
+			  writer.write("Share price: ");
+			  writer.write(Double.toString(SQL.getStock(text).getPrice()));
+		
+		
+		//**Confirmation of exit
 			System.out.println("Do you want to search for another ticker?  Type Yes or No");
-			//Commenting out in order to test File input
 			controller = sc.nextLine();	
 		}
 		}
 		finally { 
 			sc.close();
+			writer.close();
+			scanner.close();
 			//scanner.close(); // Put this call in a finally block
 		}
 		System.out.println("Goodbye");
